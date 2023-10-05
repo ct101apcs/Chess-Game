c49eaf01df0ed7d1dd84ff5be6ce1265ae1dcf4b
ct101apcs <npctri.cs22@gmail.com>
2023-10-05 10:51:57 +0700

    Generally finish 'updateValidMovesVector' for all kinds of pieces

--- GUI/ChessPieces/ChessPieces.h
+++ GUI/ChessPieces/ChessPieces.h
@@ -110,12 +110,12 @@ class Knight : public ChessPiece {
         validMoves.resize(8, std::pair<int, int>({-1, -1}));
         int row = getCurrentRow(), col = getCurrentCol();
 
-        int offsets[8][2] = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2},
-                             {1, -2},  {1, 2},  {2, -1},  {2, 1}};
+        int directions[8][2] = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2},
+                                {1, -2},  {1, 2},  {2, -1},  {2, 1}};
 
         for (int i = 0; i < 8; ++i) {
-            int newRow = row + offsets[i][0];
-            int newCol = col + offsets[i][1];
+            int newRow = row + directions[i][0];
+            int newCol = col + directions[i][1];
 
             if (newRow >= 0 && newRow < BOARD_SIZE && newCol >= 0 &&
                 newCol < BOARD_SIZE) {
@@ -297,7 +297,85 @@ class Pawn : public ChessPiece {
     }
 
     void updateValidMovesVector(const std::vector<std::vector<ChessPiece*>>& board
-    ) override {}
+    ) override {
+        validMoves.resize(6, std::pair<int, int>({-1, -1}));
+        int row = getCurrentRow(), col = getCurrentCol();
+
+        int whiteDirections[4][2] = {{-1, 1}, {1, 1}, {2, 0}, {1, 0}};
+        int blackDirections[4][2] = {{-1, -1}, {-1, 1}, {-2, 0}, {-1, 0}};
+
+        if (this->getColor() == PieceColor::White) {
+            for (int i = (this->getCurrentRow() == 1 ? 2 : 3); i < 4; ++i) {
+                int newRow = row + whiteDirections[i][0];
+                int newCol = col + whiteDirections[i][1];
+
+                if (newRow >= 0 && newRow < BOARD_SIZE && newCol >= 0 &&
+                    newCol < BOARD_SIZE) {
+                    if (board[newRow][newCol] == nullptr ||
+                        board[newRow][newCol]->getColor() != this->getColor()) {
+                        validMoves.emplace_back(newRow, newCol);
+                    }
+
+                    if (board[newRow][newCol] != nullptr) {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            }
+            for (int i = 0; i < 2; ++i) {
+                int newRow = row + whiteDirections[i][0];
+                int newCol = col + whiteDirections[i][1];
+
+                if (newRow >= 0 && newRow < BOARD_SIZE && newCol >= 0 &&
+                    newCol < BOARD_SIZE) {
+                    if (board[newRow][newCol] == nullptr) {
+                        continue;
+                    }
+                    if (board[newRow][newCol]->getColor() != this->getColor()) {
+                        validMoves.emplace_back(newRow, newCol);
+                    }
+                } else {
+                    continue;
+                }
+            }
+        } else {
+            for (int i = (this->getCurrentRow() == 6 ? 2 : 3); i < 4; ++i) {
+                int newRow = row + blackDirections[i][0];
+                int newCol = col + blackDirections[i][1];
+
+                if (newRow >= 0 && newRow < BOARD_SIZE && newCol >= 0 &&
+                    newCol < BOARD_SIZE) {
+                    if (board[newRow][newCol] == nullptr ||
+                        board[newRow][newCol]->getColor() != this->getColor()) {
+                        validMoves.emplace_back(newRow, newCol);
+                    }
+
+                    if (board[newRow][newCol] != nullptr) {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            }
+            for (int i = 0; i < 2; ++i) {
+                int newRow = row + blackDirections[i][0];
+                int newCol = col + blackDirections[i][1];
+
+                if (newRow >= 0 && newRow < BOARD_SIZE && newCol >= 0 &&
+                    newCol < BOARD_SIZE) {
+                    if (board[newRow][newCol] == nullptr) {
+                        continue;
+                    }
+                    if (board[newRow][newCol]->getColor() != this->getColor()) {
+                        validMoves.emplace_back(newRow, newCol);
+                    }
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
 
     QIcon getIcon() const override {
         if (getColor() == PieceColor::White) {
